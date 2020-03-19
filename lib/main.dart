ab4df3883d5fca60d7bd9f570e927bf5c9c8ed76
Marcos Reis <marcossummers@gmail.com>
2020-03-19 18:34:04 -0300

    Aplicação finalizada

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:alcool_gasolina/pages/home.page.dart';
+import 'package:alcool_gasolina/widgets/logo.widgets.dart';
+import 'package:alcool_gasolina/widgets/submit-form.dart';
+import 'package:alcool_gasolina/widgets/success.widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: HomePage(),
+    );
+  }
+}
